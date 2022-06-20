92aa65d0794c525f7306a4bb6f9629c07aa14853
charweeeee <you@example.comecho>
2022-06-20 17:35:49 +0100

    added files

---
+++ config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-inline-comments */
+
+module.exports = {
+  port: 8000, // Pport to listen on
+  id: "988165464821727312", // Discord Bot ID
+  usingCustomDomain: false, // Use a custom domain for the bot
+  domain: "http://localhost", // Custom domain
+  discordInvite: "https://discord.gg/yEPQkx2QvD", // Discord Invite URLL
+  clientSecret: "agP9pow-1jYPZ4DN1gni8KsgnxqMDN_9", // Client Secret from Discord Application
+  token: "OTg4MTY1NDY0ODIxNzI3MzEy.G8CUOY.JdgRygylMr6WVyqkl_rImr1XHfGOeVPHtZRNdg", // Bot Token from Discord Application
+};
+
+/**
+ * !!!
+ * You need to add a redirect url to https://discordapp.com/developers/applications/ID/oauth2.
+ * Format is: domain:port/callback example http://localhost:8080/callback
+ * - Do not include port if the port is 80.
+ * !!!
+ */
---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+  > Index.Js is the entry point of our application.
+*/
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow-restricted-names */
+
+// We import the modules.
+const config = require("./config");
+const mongoose = require("mongoose");
+const GuildSettings = require("./models/settings");
+const Dashboard = require("./dashboard/dashboard");
+const { Client, Intents, Permissions } = require("discord.js");
+
+// We instiate the client and connect to database.
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+
+
+client.config = config;
+
+
+
+  Dashboard(client);
+
+ 
+
+client.login(config.token)
+
